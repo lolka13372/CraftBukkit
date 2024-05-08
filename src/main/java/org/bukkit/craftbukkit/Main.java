aeda4fa4669afbc40cd1c61eaff6efc542f2e752
lolka13372 <f.leha2015@yandex.ru>
2024-05-09 00:43:54 +0300

    Fixed comment 2

--- src/main/java/org/bukkit/craftbukkit/Main.java
+++ src/main/java/org/bukkit/craftbukkit/Main.java
@@ -200,7 +200,7 @@ public class Main {
                 if (options.has("noconsole")) {
                     useConsole = false;
                 }
-                // Deleted time waiting
+                // Removed time waiting
                 System.out.println("Loading libraries, please wait...");
                 net.minecraft.server.Main.main(options);
             } catch (Throwable t) {
